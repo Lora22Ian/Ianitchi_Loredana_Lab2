eb56834f786ee85776d3a37c10d56ac92102e418
LOREDANA IANIȚCHI <loredana.ianitchi@stud.ubbcluj.ro>
2023-10-30 02:16:23 +0200

    Add project files.

---
+++ Data/Ianitchi_Loredana_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ianitchi_Loredana_Lab2.Models;
+
+namespace Ianitchi_Loredana_Lab2.Data
+{
+    public class Ianitchi_Loredana_Lab2Context : DbContext
+    {
+        public Ianitchi_Loredana_Lab2Context (DbContextOptions<Ianitchi_Loredana_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ianitchi_Loredana_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Ianitchi_Loredana_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Ianitchi_Loredana_Lab2.Models.Author>? Author { get; set; }
+    }
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,9 @@
+﻿namespace Ianitchi_Loredana_Lab2.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
